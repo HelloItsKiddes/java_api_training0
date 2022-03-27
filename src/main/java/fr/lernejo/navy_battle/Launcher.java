9e1f3875cf40ab23563501631b93577496ee8803
HelloItsKiddes <aboubekre.sayoud@gmail.com>
2022-03-27 10:14:01 +0200

    Added Ping

---
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -0,0 +1,10 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpServer;
+
+
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+        Serveur serv = new Serveur();
+    }
+}
